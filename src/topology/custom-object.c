71837152ac5c139198302a0a4cdc407bbbe513e5
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2021-03-22 17:00:55 -0700

    topology2: update automatic attributes for buffer class
    The buffer class's "size" attribute is an autmatic attribute.
    It needs to computed using the pipeline attributes along with
    some of the buffer object attributes. Add support for updating
    it.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

--- src/topology/custom-object.c
+++ src/topology/custom-object.c
@@ -74,6 +74,120 @@ int tplg_create_pipeline_object(struct tplg_class *class, struct tplg_object *ob
 	return 0;
 }
 
+static int tplg_get_sample_size_from_format(char *format)
+{
+	if (!strcmp(format, "s32le") || !strcmp(format, "s24le") || !strcmp(format, "float") )
+		return 4;
+
+	if (!strcmp(format, "s16le"))
+		return 2;
+
+	return -EINVAL;
+}
+
+static int tplg_update_buffer_size(struct tplg_object *buffer_object,
+				    struct tplg_object *pipeline_object)
+{
+	struct list_head *pos;
+	struct tplg_attribute *size_attribute = NULL;
+	char pipeline_format[SNDRV_CTL_ELEM_ID_NAME_MAXLEN];
+	int periods = 0;
+	int sample_size;
+	int channels = 0;
+	int frames = 0;
+	int rate = 0;
+	int schedule_period = 0;
+
+	/* get periods and channels from buffer object */
+	list_for_each(pos, &buffer_object->attribute_list) {
+		struct tplg_attribute *attr = list_entry(pos, struct tplg_attribute, list);
+
+		if (!strcmp(attr->name, "periods")) {
+			if (attr->type == SND_CONFIG_TYPE_INTEGER) {
+				periods = attr->value.integer;
+			} else {
+				SNDERR("Invalid value for periods for object %s \n",
+				       buffer_object->name);
+				return -EINVAL;
+			}
+		}
+
+		if (!strcmp(attr->name, "channels")) {
+			if (attr->type == SND_CONFIG_TYPE_INTEGER) {
+				channels = attr->value.integer;
+			} else {
+				SNDERR("Invalid value for channels for object %s \n",
+				       buffer_object->name);
+				return -EINVAL;
+			}
+		}
+
+		if (!strcmp(attr->name, "size"))
+			size_attribute = attr;
+	}
+
+	if (!size_attribute) {
+		SNDERR("Can't find size attribute for %s\n", buffer_object->name);
+		return -EINVAL;
+	}
+
+	/* get schedule_period, channels, rate and format from pipeline object */
+	list_for_each(pos, &pipeline_object->attribute_list) {
+		struct tplg_attribute *attr = list_entry(pos, struct tplg_attribute, list);
+
+		if (!strcmp(attr->name, "period")) {
+			if (attr->type == SND_CONFIG_TYPE_INTEGER) {
+				schedule_period = attr->value.integer;
+			} else {
+				SNDERR("Invalid value for period for object %s \n",
+				       pipeline_object->name);
+				return -EINVAL;
+			}
+		}
+
+		if (!strcmp(attr->name, "rate")) {
+			if (attr->type == SND_CONFIG_TYPE_INTEGER) {
+				rate = attr->value.integer;
+			} else {
+				SNDERR("Invalid value for rate for object %s \n",
+				       pipeline_object->name);
+				return -EINVAL;
+			}
+		}
+
+		if (!strcmp(attr->name, "format")) {
+			if (attr->type == SND_CONFIG_TYPE_STRING) {
+				snd_strlcpy(pipeline_format, attr->value.string,
+					    SNDRV_CTL_ELEM_ID_NAME_MAXLEN);
+			} else {
+				SNDERR("Invalid format for pipeline %s \n",
+				       pipeline_object->name);
+				return -EINVAL;
+			}
+		}
+	}
+
+	sample_size = tplg_get_sample_size_from_format(pipeline_format);
+	if (sample_size < 0) {
+		SNDERR("Invalid value for sample size for object %s \n", pipeline_object->name);
+		return sample_size;
+	}
+
+	/* compute buffer size */
+	frames = (rate * schedule_period)/1000000;
+	size_attribute->value.integer = periods * sample_size * channels * frames;
+	if (!size_attribute->value.integer) {
+		SNDERR("Invalid buffer size %d for %s \n",size_attribute->value.integer,
+		       buffer_object->name);
+		return -EINVAL;
+	}
+
+	size_attribute->found = true;
+	size_attribute->type = SND_CONFIG_TYPE_INTEGER;
+
+	return 0;
+}
+
 int tplg_update_automatic_attributes(snd_tplg_t *tplg, struct tplg_object *object,
 				     struct tplg_object *parent)
 {
@@ -83,6 +197,14 @@ int tplg_update_automatic_attributes(snd_tplg_t *tplg, struct tplg_object *objec
 	if (!strcmp(object->class_name, "host") ||
 	    !strcmp(object->class_name, "copier")) {
 		tplg_set_stream_name(object);
+	}
+
+	if (!strcmp(object->class_name, "buffer")) {
+		if (parent) {
+			ret =  tplg_update_buffer_size(object, parent);
+			if (ret < 0)
+				return 0;
+		}
 	}
 
 	/* now update all automatic attributes for all child objects */
